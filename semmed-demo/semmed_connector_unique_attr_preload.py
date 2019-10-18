075b14c54737772ae2bb14483cfcd04a5e9b0272
Joshua Send <joshuasend1@gmail.com>
2019-10-18 16:23:05 +0100

    fix non-unique attr problem due to DISTINCT and cases in mysql

--- semmed-demo/semmed_connector_unique_attr_preload.py
+++ semmed-demo/semmed_connector_unique_attr_preload.py
@@ -121,7 +121,8 @@ def fetch_unique_attributes(mydb, start_index, end_index):
         "SENTENCE": "sentence-text"
     }
 
-    sql_attr_query = "SELECT DISTINCT {0} FROM SENTENCE WHERE SENTENCE_ID < " + str(end_index) + " AND SENTENCE_ID >= " + str( start_index) + ";"
+    # turns out DISTINCT is not case sensitive!! use hash first to handle all weird character cases too
+    sql_attr_query = "SELECT DISTINCT(MD5({0})) FROM SENTENCE WHERE SENTENCE_ID < " + str(end_index) + " AND SENTENCE_ID >= " + str( start_index) + ";"
     for sql_attribute in sql_to_grakn_attributes:
 
         grakn_attribute_type = sql_to_grakn_attributes[sql_attribute]
